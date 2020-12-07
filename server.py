2283a5b430dc073100841c08b4077d149c97ebbd
katiepalani <75643867+katiepalani@users.noreply.github.com>
2020-12-07 15:49:07 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)    
+
+
+# contact 
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submit from'
